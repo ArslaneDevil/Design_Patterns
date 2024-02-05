f1dcf54df8f8652c4815506259bc28051169a904
ArslaneDevil <arslaneben08@gmail.com>
2024-02-05 11:33:41 +0100

    new file 'TodoCommandHandler.java' 05/02/24

---
+++ src/main/java/com/fges/todoapp/TodoCommandHandler.java
@@ -0,0 +1,37 @@
+package com.fges.todoapp;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class TodoCommandHandler {
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+
+    public TodoCommandHandler(String fileName) {
+        Path filePath = Paths.get(fileName);
+        if (fileName.endsWith(".json")) {
+            this.fileReader = new JsonFileReader(filePath);
+            this.fileWriter = new JsonFileWriter(filePath);
+        } else if (fileName.endsWith(".csv")) {
+            // Initialize CSV reader and writer
+            // this.fileReader = new CsvFileReader(filePath);
+            // this.fileWriter = new CsvFileWriter(filePath);
+        }
+    }
+
+    public void executeCommand(String command, String[] args) throws IOException {
+        switch (command) {
+            case "insert":
+                if (args.length < 1) throw new IllegalArgumentException("Missing TODO name");
+                String todo = args[0];
+                fileWriter.writeTodo(todo);
+                break;
+            case "list":
+                fileReader.readTodos().forEach(System.out::println);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown command: " + command);
+        }
+    }
+}
