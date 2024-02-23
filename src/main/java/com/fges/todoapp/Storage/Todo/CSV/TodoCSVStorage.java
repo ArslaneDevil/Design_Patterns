d27e3eeff4e10b82e8a3d7108800d0bc6d78ac9e
ArslaneDevil <arslaneben08@gmail.com>
2024-02-23 16:05:00 +0100

    new structure  23/02/24

--- src/main/java/com/fges/todoapp/Storage/Todo/CSV/TodoCSVStorage.java
+++
@@ -1,4 +0,0 @@
-package com.fges.todoapp.Storage.Todo.CSV;
-
-public class TodoCSVStorage {
-}
--- src/main/java/com/fges/todoapp/Storage/Todo/service/TodoStorage.java
+++
@@ -1,4 +0,0 @@
-package com.fges.todoapp.Storage.Todo.service;
-
-public interface TodoStorage extends TodoReaderStorage, TodoWriterStorage{
-}
